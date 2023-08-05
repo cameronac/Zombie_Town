e914285fd2671c5380a6f6d2d194d5c8e78e8770
Donald Slebodnick <112369183+Dark1214@users.noreply.github.com>
2023-08-05 19:07:51 -0400

    Added player tag and playerScript to the gameManager

--- Assets/Interfaces/gameManager.cs
+++ Assets/Interfaces/gameManager.cs
@@ -7,16 +7,25 @@ public class gameManager : MonoBehaviour
     public static gameManager instance;
 
     public GameObject player;
+    public playerState playerScript;
+
+    public GameObject pauseMenu;
+
+    bool isPaused;
     // Start is called before the first frame update
     void Awake()
     {
         instance = this;
         player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<playerState>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Cancel"))
+        {
+            isPaused = !isPaused;
+        }
     }
 }
