fe491337729ea9484bd448e206247015e3a44e07
Donald Slebodnick <112369183+Dark1214@users.noreply.github.com>
2023-08-08 21:29:22 -0400

    Door prefab done and win condition

---
+++ Assets/Prefabs/UserInterface/Script/doorEscape.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorEscape : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        gameManager.instance.youWin();
+    }
+}
