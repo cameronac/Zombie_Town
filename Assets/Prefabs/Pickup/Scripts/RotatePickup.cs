43247bfb967b5608dd8ba1e2270ca555a8aee947
Adam Mashaw <mashaw102@gmail.com>
2023-08-15 11:29:47 -0700

    Test Commit
    Hopefully this has my actual name

--- Assets/Prefabs/Pickup/Scripts/RotatePickup.cs
+++ Assets/Prefabs/Pickup/Scripts/RotatePickup.cs
@@ -8,6 +8,7 @@ public class RotatePickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15,30,45) * Time.deltaTime);       
+        transform.Rotate(new Vector3(15,30,45) * Time.deltaTime);    
+        
     }
 }
