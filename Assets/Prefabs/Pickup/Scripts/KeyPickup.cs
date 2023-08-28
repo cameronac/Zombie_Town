514c1bcd3f597e523cbaa6bac18ed7ca89b57760
Adam Mashaw <mashaw102@gmail.com>
2023-08-28 08:17:42 -0700

    last update

--- Assets/Prefabs/Pickup/Scripts/KeyPickup.cs
+++ Assets/Prefabs/Pickup/Scripts/KeyPickup.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class PickKeyPickup : MonoBehaviour
 {
     [SerializeField] int ID;
+    [SerializeField] string txt;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.TryGetComponent(out IPickup isPickup))
         {
             isPickup.PickupKeyItem(ID);
-            gameManager.instance.updateObjective("Escape!");
+            gameManager.instance.updateObjective(txt);
             Destroy(gameObject);
         }
     }
