5e8de4d47e3395245a4193b3fc6e0441864a1640
SpeckofDust25 <cameroncollins19@yahoo.com>
2023-09-06 19:46:19 -0500

    WIP Audio Manager

--- Assets/Singletons/audioManager.cs
+++ Assets/Singletons/audioManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class audioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    float volume = 1.0f;
+
+    [SerializeField] GameObject audioPrefab;
+    public static audioManager instance;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    public void CreateSoundAtPosition(AudioClip clip, Vector3 position)
+    {
+        GameObject prefab = Instantiate(audioPrefab, position, Quaternion.identity);
+        AudioSource audioSource = prefab.GetComponent<AudioSource>();
+        audioSource.volume = volume;
+        audioSource.clip = clip;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateOneDimensionalSound(AudioClip clip)
     {
-        
+        GameObject prefab = Instantiate(audioPrefab);
+        AudioSource audioSource = prefab.GetComponent<AudioSource>();
+        audioSource.volume = volume;
+        audioSource.clip = clip;
     }
 }
