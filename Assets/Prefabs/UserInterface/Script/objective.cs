39b6326efc5e854871dc38a05167b1648d67d95b
sandyboyett <SBoyett@student.fullsail.edu>
2023-09-13 21:27:29 -0400

    minor objective changes

--- Assets/Prefabs/UserInterface/Script/objective.cs
+++ Assets/Prefabs/UserInterface/Script/objective.cs
@@ -7,20 +7,31 @@ using UnityEngine;
 public class Objective : MonoBehaviour
 {
     [SerializeField] string currentObjective;
-    //[SerializeField] string newObjective;
-
-    //private void Start()
-    //{
-    //    gameManager.instance.updateObjective(currentObjective);
-    //    Destroy(gameObject);
-    //}
 
     void OnTriggerEnter(Collider other) 
     {
         if (other.gameObject.tag == "Player")
         {
+            //call check completion
+
             gameManager.instance.updateObjective(currentObjective);
             Destroy(gameObject);
         }
+    }
+
+    void CheckCompletion()
+    {
+        //if objective is completed, call completed objective and current objective
+        //otherwise call not completed
+    }
+
+    void CompletedObjective()
+    {
+        //current objective will changed to striked text
+    }
+
+    void NotCompleted()
+    {
+        //current objective will be displayed again as reminder to the player to complete before they can proceed
     }
 }
