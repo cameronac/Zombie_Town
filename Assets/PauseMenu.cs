9508788cc150b234ad9fc0d25840c2e79481bcad
sandyboyett <SBoyett@student.fullsail.edu>
2023-09-14 20:22:04 -0400

    minor changes with objectives

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -24,7 +24,6 @@ public class PauseMenu : MonoBehaviour
         if (Input.GetButtonDown("Submit"))
         {
             ButtonPressed(index);
-            
         }
 
         bool up = Input.GetButtonDown("Up");
