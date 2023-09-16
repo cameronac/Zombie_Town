141b8f0a3be46d8f47e61816cc226ac76f48600c
Adam Mashaw <mashaw102@gmail.com>
2023-09-16 14:19:40 -0700

    Vsync and Fullscreen Functionality

--- Assets/systemMenu.cs
+++ Assets/systemMenu.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class systemMenu : MonoBehaviour
 {
+    public Toggle isFullscreen;
+    public Toggle isVsync;
+    public void Start()
+    {
+        isFullscreen.isOn = Screen.fullScreen;
+
+        if (QualitySettings.vSyncCount == 0)
+            isVsync.isOn = false;
+        if (QualitySettings.vSyncCount == 1)
+            isVsync.isOn = true;
+    }
     public void setMusicVolume(float volume)
     {
         AudioManager.music_volume = volume;
@@ -17,5 +29,22 @@ public class systemMenu : MonoBehaviour
     public void setSfxVolume(float volume)
     {
         AudioManager.sfx_volume = volume;
+    }
+
+    public void setFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void setVsync(bool isVsync)
+    {
+        if(isVsync)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
     }
 }
