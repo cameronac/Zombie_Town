85c35205f617e31f5629779b45e34dee787497e9
Adam Mashaw <mashaw102@gmail.com>
2023-09-21 11:55:19 -0700

    Added hit registration for objects

--- Assets/Prefabs/Interactions/DestructibleObject.cs
+++ Assets/Prefabs/Interactions/DestructibleObject.cs
@@ -7,9 +7,17 @@ public class DestructableObject : MonoBehaviour, IDamage
     [SerializeField] float hp;
     [SerializeField] AudioClip hit_sound;
     [SerializeField] AudioClip destroy_sound;
+    [SerializeField] Transform[] allChildren;
+    int index;
 
     public void TakeDamage(float damage)
     {
+        allChildren = GetComponentsInChildren<Transform>();
+        List<GameObject> childObjects = new List<GameObject>();
+        for(int i = 1; i < allChildren.Length; i++)
+        {
+            childObjects.Add(allChildren[i].gameObject);
+        }
         hp -= damage;
 
         if (hp <= 0)
@@ -17,9 +25,12 @@ public class DestructableObject : MonoBehaviour, IDamage
             playerState.instance.destroyItems.Add(gameObject.name);
             AudioManager.instance.CreateSoundAtPosition(destroy_sound, transform.position);
             Destroy(gameObject);
-        } else
+        }
+        else
         {
             AudioManager.instance.CreateSoundAtPosition(hit_sound, transform.position);
+            index = Random.Range(0, childObjects.Count);
+            Destroy(childObjects[index]);
         }
     }
 }
