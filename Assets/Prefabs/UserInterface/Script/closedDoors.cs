3bf7270ca1c5541bf9826ea15a7794e3a564a534
Adam Mashaw <mashaw102@gmail.com>
2023-09-21 18:20:19 -0700

    Fixed interact with nothingDoor

--- Assets/Prefabs/UserInterface/Script/closedDoors.cs
+++ Assets/Prefabs/UserInterface/Script/closedDoors.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class closedDoors : MonoBehaviour
+public class closedDoors : MonoBehaviour, IInteract
 {
-    private void OnTriggerEnter(Collider other)
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.GetComponent("playerState") as playerState != null)
+    //    {
+    //        gameManager.instance.updateObjective("Something must be blocking it from the inside");
+    //    }
+    //}
+
+    void IInteract.buttonPressed()
     {
-        if (other.gameObject.GetComponent("playerState") as playerState != null)
-        {
-            gameManager.instance.updateObjective("Something must be blocking it from the inside");
-        }
+        gameManager.instance.updateObjective("Something must be blocking it from the inside");
     }
 }
